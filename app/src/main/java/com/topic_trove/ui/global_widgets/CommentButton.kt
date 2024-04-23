641694b7b4ef8aae78e8b3b6b35d751bb382171b
DQP-UIT <22521149@gm.uit.edu.vn>
2024-04-23 11:55:27 +0700

    PostCard component

--- app/src/main/java/com/topic_trove/ui/global_widgets/CommentButton.kt
+++ app/src/main/java/com/topic_trove/ui/global_widgets/CommentButton.kt
@@ -1,69 +1,18 @@
 package com.topic_trove.ui.global_widgets
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.drawable.Drawable
-import android.text.TextPaint
 import android.util.AttributeSet
-import android.view.View
+import android.widget.Button
 import com.topic_trove.R
 
-/**
- * TODO: document your custom view class.
- */
-class CommentButton : View {
-
-    private var _exampleString: String? = null // TODO: use a default from R.string...
-    private var _exampleColor: Int = Color.RED // TODO: use a default from R.color...
-    private var _exampleDimension: Float = 0f // TODO: use a default from R.dimen...
-
-    private lateinit var textPaint: TextPaint
-    private var textWidth: Float = 0f
-    private var textHeight: Float = 0f
-
-    /**
-     * The text to draw
-     */
-    var exampleString: String?
-        get() = _exampleString
-        set(value) {
-            _exampleString = value
-            invalidateTextPaintAndMeasurements()
-        }
-
-    /**
-     * The font color
-     */
-    var exampleColor: Int
-        get() = _exampleColor
-        set(value) {
-            _exampleColor = value
-            invalidateTextPaintAndMeasurements()
-        }
-
-    /**
-     * In the example view, this dimension is the font size.
-     */
-    var exampleDimension: Float
-        get() = _exampleDimension
-        set(value) {
-            _exampleDimension = value
-            invalidateTextPaintAndMeasurements()
-        }
-
-    /**
-     * In the example view, this drawable is drawn above the text.
-     */
-    var exampleDrawable: Drawable? = null
+class CommentButton : Button {
 
     constructor(context: Context) : super(context) {
-        init(null, 0)
+        init(context, null, 0)
     }
 
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
-        init(attrs, 0)
+        init(context, attrs, 0)
     }
 
     constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(
@@ -71,87 +20,30 @@ class CommentButton : View {
         attrs,
         defStyle
     ) {
-        init(attrs, defStyle)
+        init(context, attrs, defStyle)
     }
 
-    private fun init(attrs: AttributeSet?, defStyle: Int) {
-        // Load attributes
-        val a = context.obtainStyledAttributes(
-            attrs, R.styleable.CommentButton, defStyle, 0
-        )
+    private fun init(context: Context, attrs: AttributeSet?, defStyle: Int) {
+        // Apply attributes from XML layout
+        attrs?.let {
+            val typedArray = context.obtainStyledAttributes(it, R.styleable.CommentButton, defStyle, 0)
 
-        _exampleString = a.getString(
-            R.styleable.CommentButton_exampleString
-        )
-        _exampleColor = a.getColor(
-            R.styleable.CommentButton_exampleColor,
-            exampleColor
-        )
-        // Use getDimensionPixelSize or getDimensionPixelOffset when dealing with
-        // values that should fall on pixel boundaries.
-        _exampleDimension = a.getDimension(
-            R.styleable.CommentButton_exampleDimension,
-            exampleDimension
-        )
+            val drawableStart = typedArray.getResourceId(R.styleable.CommentButton_iconsource, -1)
+            if (drawableStart != -1) {
+                setCompoundDrawablesWithIntrinsicBounds(drawableStart, 0, 0, 0)
+            }
 
-        if (a.hasValue(R.styleable.CommentButton_exampleDrawable)) {
-            exampleDrawable = a.getDrawable(
-                R.styleable.CommentButton_exampleDrawable
-            )
-            exampleDrawable?.callback = this
-        }
+            val background = typedArray.getResourceId(R.styleable.CommentButton_border_style, -1)
+            if (background != -1) {
+                setBackgroundResource(background)
+            }
 
-        a.recycle()
+            val text = typedArray.getString(R.styleable.CommentButton_text)
+            text?.let {
+                setText(it)
+            }
 
-        // Set up a default TextPaint object
-        textPaint = TextPaint().apply {
-            flags = Paint.ANTI_ALIAS_FLAG
-            textAlign = Paint.Align.LEFT
-        }
-
-        // Update TextPaint and text measurements from attributes
-        invalidateTextPaintAndMeasurements()
-    }
-
-    private fun invalidateTextPaintAndMeasurements() {
-        textPaint.let {
-            it.textSize = exampleDimension
-            it.color = exampleColor
-            textWidth = it.measureText(exampleString)
-            textHeight = it.fontMetrics.bottom
-        }
-    }
-
-    override fun onDraw(canvas: Canvas) {
-        super.onDraw(canvas)
-
-        // TODO: consider storing these as member variables to reduce
-        // allocations per draw cycle.
-        val paddingLeft = paddingLeft
-        val paddingTop = paddingTop
-        val paddingRight = paddingRight
-        val paddingBottom = paddingBottom
-
-        val contentWidth = width - paddingLeft - paddingRight
-        val contentHeight = height - paddingTop - paddingBottom
-
-        exampleString?.let {
-            // Draw the text.
-            canvas.drawText(
-                it,
-                paddingLeft + (contentWidth - textWidth) / 2,
-                paddingTop + (contentHeight + textHeight) / 2,
-                textPaint
-            )
-        }
-
-        // Draw the example drawable on top of the text.
-        exampleDrawable?.let {
-            it.setBounds(
-                paddingLeft, paddingTop,
-                paddingLeft + contentWidth, paddingTop + contentHeight
-            )
-            it.draw(canvas)
+            typedArray.recycle()
         }
     }
 }
--- app/src/main/java/com/topic_trove/ui/global_widgets/PostCard.kt
+++ app/src/main/java/com/topic_trove/ui/global_widgets/PostCard.kt
@@ -13,43 +13,51 @@ class PostCard : LinearLayout {
 
     private var textHashtag: TextView
     private var imageAvatar: ImageView
+    private var imagetick: ImageView
     private var textName: TextView
     private var textDate: TextView
     private var textContent: TextView
     private var btnLike: LikeButton
     private var btnComment: CommentButton
+    private var textTitle: TextView
 
     constructor(context: Context) : super(context) {
         LayoutInflater.from(context).inflate(R.layout.sample_post_card, this, true)
         textHashtag = findViewById(R.id.textHashtag)
         imageAvatar = findViewById(R.id.imageAvatar)
+        imagetick = findViewById(R.id.imageVerificationTick)
         textName = findViewById(R.id.textName)
         textDate = findViewById(R.id.textDate)
         textContent = findViewById(R.id.textContent)
         btnLike = findViewById(R.id.btnLike)
         btnComment = findViewById(R.id.btnComment)
+        textTitle = findViewById(R.id.textTitle)
     }
 
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
         LayoutInflater.from(context).inflate(R.layout.sample_post_card, this, true)
         textHashtag = findViewById(R.id.textHashtag)
         imageAvatar = findViewById(R.id.imageAvatar)
+        imagetick = findViewById(R.id.imageVerificationTick)
         textName = findViewById(R.id.textName)
         textDate = findViewById(R.id.textDate)
         textContent = findViewById(R.id.textContent)
         btnLike = findViewById(R.id.btnLike)
         btnComment = findViewById(R.id.btnComment)
+        textTitle = findViewById(R.id.textTitle)
     }
 
     constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle) {
         LayoutInflater.from(context).inflate(R.layout.sample_post_card, this, true)
         textHashtag = findViewById(R.id.textHashtag)
         imageAvatar = findViewById(R.id.imageAvatar)
+        imagetick = findViewById(R.id.imageVerificationTick)
         textName = findViewById(R.id.textName)
         textDate = findViewById(R.id.textDate)
         textContent = findViewById(R.id.textContent)
         btnLike = findViewById(R.id.btnLike)
         btnComment = findViewById(R.id.btnComment)
+        textTitle = findViewById(R.id.textTitle)
     }
 
     fun setHashtag(hashtag: String) {
@@ -60,6 +68,9 @@ class PostCard : LinearLayout {
         imageAvatar.setImageDrawable(avatarDrawable)
     }
 
+    fun setVerificationTick(verificationDrawable: Drawable){
+        imagetick.setImageDrawable(verificationDrawable)
+    }
     fun setName(name: String) {
         textName.text = name
     }
@@ -68,6 +79,9 @@ class PostCard : LinearLayout {
         textDate.text = date
     }
 
+    fun setTitle(title: String) {
+        textTitle.text = title
+    }
     fun setContent(content: String) {
         textContent.text = content
     }
