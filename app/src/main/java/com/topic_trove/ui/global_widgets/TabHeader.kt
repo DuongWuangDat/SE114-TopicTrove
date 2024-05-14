8a61274f990a3707951c1701b6d166f9b384f44b
DuongWuangDat <duongquangdat172004@gmail.com>
2024-05-14 11:44:16 +0700

    fix

--- app/src/main/java/com/topic_trove/ui/global_widgets/TabHeader.kt
+++ app/src/main/java/com/topic_trove/ui/global_widgets/TabHeader.kt
@@ -13,7 +13,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.topic_trove.ui.modules.example.TabHeader
 import com.topic_trove.ui.core.values.AppColors
 
 @Composable
